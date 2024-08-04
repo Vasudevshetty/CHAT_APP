035ddf4347c0ae48c941413941f9e77d41df4290
Vasudevshetty <vasudeepu2815@gmail.com>
2024-08-04 17:30:43 +0530

    Created 2 more controllers one to check the password and other to check the email.

---
+++ server/controllers/checkPassword.js
@@ -0,0 +1,44 @@
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+
+async function checkPassword(req, res) {
+  try {
+    const { password, userId } = req.body;
+    const user = await User.findById(userId);
+
+    const verifyPassword = await bcryptjs.compare(password, user.password);
+
+    if (!verifyPassword)
+      return res.status(400).json({
+        message: "Please check password",
+        error: true,
+      });
+
+    const tokenData = {
+      id: user._id,
+      email: user.email,
+    };
+    const token = await jwt.sign(tokenData, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    const cookieOption = {
+      http: true,
+      secure: true,
+    };
+
+    return res.cookie('token', token, cookieOption).status(200).json({
+      message: "Login successful",
+      data: user,
+      success: true,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message || err,
+      error: true,
+    });
+  }
+}
+
+module.exports = checkPassword;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const regiseterUser = require("../controllers/registerUser");
+const checkEmail = require("../controllers/checkEmail");
+const checkPassword = require("../controllers/checkPassword");
 
 const router = express.Router();
 
-// api endpoints to register
+// api endpoints
+
+// create a new user
 router.post("/register", regiseterUser);
+// check user email
+router.post("/email", checkEmail);
+// check password and login
+router.post("/password", checkPassword)
 
 module.exports = router;
