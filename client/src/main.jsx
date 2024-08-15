0bb6b8d70ef773ff2f94cc75e6519ed94e1ff85a
Vasudevshetty <vasudeepu2815@gmail.com>
2024-08-15 19:08:51 +0530

    Redux setup done, will complete remaining work

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 import router from "./routes/index.jsx";
 import App from "./App.jsx";
 import "./index.css";
+import { store } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <Provider store={store}>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </Provider>
   </React.StrictMode>
 );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,24 @@
 import { Outlet } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useEffect } from "react";
 
 function Home() {
+  async function fetchUser() {
+    try {
+      const response = await axios({
+        url: `${import.meta.env.VITE_APP_BACKEND_URL_DEV}/user-details`,
+        withCredentials: true,
+      });
+      console.log(response);
+    } catch (error) {
+      toast(error.message);
+    }
+  }
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       Home
